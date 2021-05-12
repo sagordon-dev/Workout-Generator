0116589959f5eb5cecf13e0628872834732b950a
Scott Gordon <81840601+sagordon-dev@users.noreply.github.com>
2021-05-12 05:42:05 -0400

    Add workout script

--- script.py
+++ script.py
@@ -1,1 +1,21 @@
+import random
+
+back = ['bent-over row', 'lying row', ]
+quadriceps = ['lunge', 'side lunge', 'walking lunge', 'rear lunge', 'split squat',
+'front squat', 'step ups']
+chest = ['bench press', 'fly', 'pullover']
+hamstrings = ['straight-leg deadlift', 'straight-back stiff-leg deadlift']
+deltoid = ['arnold press', 'front raise', 'shoulder press']
+calves = ['standing calf raise', 'single leg calf raise']
+biceps = ['curl', 'seated curl', 'incline curl']
+abdominal = ['push crunch', 'crunch', 'jack-knife', 'reverse crunch']
+triceps = ['kickback', 'lying tricep extension', 'incline tricep extension']
+
+def randomize(lst):
+    return random.randint(1, len(lst)-1)
+
+exercise1 = randomize(back)
+
+
+print(exercise1)
 
