05780a7c914e9bdfb6acc41c831dcbda04367720
Scott Gordon <81840601+sagordon-dev@users.noreply.github.com>
2021-05-13 06:03:20 -0400

    Update script.py

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import random
 import datetime
+
 back = ['bent-over row', 'lying row', ]
 quadriceps = ['lunge', 'side lunge', 'walking lunge', 'rear lunge', 'split squat',
               'front squat', 'step ups']
@@ -10,6 +11,8 @@ calves = ['standing calf raise', 'single leg calf raise']
 biceps = ['curl', 'seated curl', 'incline curl']
 abdominal = ['push crunch', 'crunch', 'jack-knife', 'reverse crunch']
 triceps = ['kickback', 'lying tricep extension', 'incline tricep extension']
+cardio = ['skaters', 'jump jacks', 'burpees', 'jump rope', 'mountain climbers',
+          'high knees', 'fanny kickers', 'jump squats']
 
 
 def random_exercise_generator(lst):
@@ -26,6 +29,7 @@ calves_exercise = random_exercise_generator(calves)
 biceps_exercise = random_exercise_generator(biceps)
 abdominal_exercise = random_exercise_generator(abdominal)
 triceps_exercise = random_exercise_generator(triceps)
+cardio_exercise = random_exercise_generator(cardio)
 
 name = input("Name: ")
 date = datetime.datetime.now()
@@ -38,7 +42,9 @@ print(f"Quads: {quadriceps_exercise}")
 print(f"Chest: {chest_exercise}")
 print(f"Hams: {hamstrings_exercise}")
 print(f"Delts: {deltoid_exercise}")
+print(f"Calves: {calves_exercise}")
 print(f"Bis: {biceps_exercise}")
 print(f"Abs: {abdominal_exercise}")
 print(f"Tris: {triceps_exercise}")
+print(f"Cardio: {cardio_exercise}")
 print("**********************************")
